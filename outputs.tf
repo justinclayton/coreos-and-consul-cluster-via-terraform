467d0c8140d156510db1ec4979f5fa9e7712ca7c
Justin Clayton <justin@zaphod.amer.gettywan.com>
2015-01-02 14:42:50 -0800

    comment cleanup

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,3 @@ output "consul_url" {
 output "fleet_env" {
   value = "export FLEETCTL_TUNNEL=${var.stack_name}.${var.route53_domain}:2222\nexport FLEETCTL_STRICT_HOST_KEY_CHECKING=false"
 }
-
-# output "nodes" {
-#   value = "${aws_elb.coreos_leader_elb.instances.*}"
-# }
